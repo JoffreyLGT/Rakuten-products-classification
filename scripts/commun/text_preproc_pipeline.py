539742cb226c09cff67d2032f28ea49673d26c42
Heiko Agnoli <heiko.agnoli@gmail.com>
2023-03-21 09:36:17 +0100

    added fit_transform and new class with extraprepro

--- scripts/commun/text_preproc_pipeline.py
+++ scripts/commun/text_preproc_pipeline.py
@@ -8,12 +8,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 import scipy.sparse as sparse
 
 class TextPreprocess():
+    
     def __init__(self, pipeline):
         self.pipeline = pipeline
 
     def fit(self, data) -> object:
         return self.pipeline.fit(data)
-        
+
+    def fit_transform(self, data) -> object:
+        return self.pipeline.fit_transform(data)
+
     def transform(self, data) -> np.array:
         return self.pipeline.transform(data)
 
@@ -30,3 +34,18 @@ class TextPreprocess():
         with open(file_name,'wb') as fp:
             pickle.dump(voc, fp)
 
+class TPWithExtraPreproc(TextPreprocess):
+    '''
+    Example:
+        bow_preproc = TPWithExtraPreproc(TfidfV1())
+        X_train = bow_preproc.extra_preproc(X_train)
+        bow_preproc.fit(X_train)
+        ...
+    '''
+
+    def __init__(self):
+        TextPreprocess.__init__(self)
+
+    def extra_preproc(self, data):
+        # add some extra preproc
+        return data
